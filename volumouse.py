a4047b1aa2dba553178770c04f36cc396066b94d
pzim <52496172+pzim-devdata@users.noreply.github.com>
2023-03-14 23:35:00 +0100

    Add files via upload

--- volumouse.py
+++ volumouse.py
@@ -388,141 +388,144 @@ def main():
 
     try :
         f = open(file_Screen_resolution, 'r')
-        Screen_resolution = tuple(map(f.read()))
+        str_resolution=f.read().replace('(','').replace(')','')
+        Screen_resolution = tuple(map(int, str_resolution.split(',')))
         f.close()
     except :
         Screen_resolution = (1920,1080)
+        
+        print(Screen_resolution)
 
-        def volume_up_top_right():
-            subprocess.run("pactl set-sink-volume "+Top_right_card+" +5% & pid=$!",shell=True)
-            #######"IT COULD ALSO BE FOR EXEMPLE:
-            #subprocess.run("pactl set-sink-volume alsa_output.pci-0000_01_00.1.hdmi-stereo-extra3 +5% & pid=$!",shell=True)
-            #subprocess.run("pactl set-sink-volume @DEFAULT_SINK@ +5% & pid=$!",shell=True)
-            #subprocess.run("amixer set Master 5%+ & pid=$!",shell=True)
-            #subprocess.run("pactl set-sink-volume 4 +5% & pid=$!",shell=True)
-            #subprocess.run("pacmd set-sink-volume 1 10 & pid=$!",shell=True)
-            #subprocess.run("amixer -q sset Master 3%+ & pid=$!",shell=True)
+    def volume_up_top_right():
+        subprocess.run("pactl set-sink-volume "+Top_right_card+" +5% & pid=$!",shell=True)
+        #######"IT COULD ALSO BE FOR EXEMPLE:
+        #subprocess.run("pactl set-sink-volume alsa_output.pci-0000_01_00.1.hdmi-stereo-extra3 +5% & pid=$!",shell=True)
+        #subprocess.run("pactl set-sink-volume @DEFAULT_SINK@ +5% & pid=$!",shell=True)
+        #subprocess.run("amixer set Master 5%+ & pid=$!",shell=True)
+        #subprocess.run("pactl set-sink-volume 4 +5% & pid=$!",shell=True)
+        #subprocess.run("pacmd set-sink-volume 1 10 & pid=$!",shell=True)
+        #subprocess.run("amixer -q sset Master 3%+ & pid=$!",shell=True)
 
-            # use "& pid=$!" end of your command to prevent freezing python script if your command takes long time to process.
-        def volume_down_top_right():
-            subprocess.run("pactl set-sink-volume "+Top_right_card+" -5% & pid=$!",shell=True)
+        # use "& pid=$!" end of your command to prevent freezing python script if your command takes long time to process.
+    def volume_down_top_right():
+        subprocess.run("pactl set-sink-volume "+Top_right_card+" -5% & pid=$!",shell=True)
 
-        def volume_up_bottom_right():
-            subprocess.run("pactl set-sink-volume "+Bottom_right_card+" +5% & pid=$!",shell=True)
-        def volume_down_bottom_right():
-            subprocess.run("pactl set-sink-volume "+Bottom_right_card+" -5% & pid=$!",shell=True)
+    def volume_up_bottom_right():
+        subprocess.run("pactl set-sink-volume "+Bottom_right_card+" +5% & pid=$!",shell=True)
+    def volume_down_bottom_right():
+        subprocess.run("pactl set-sink-volume "+Bottom_right_card+" -5% & pid=$!",shell=True)
 
-        def volume_up_top_left():
-            subprocess.run("pactl set-sink-volume "+Top_left_card+" +5% & pid=$!",shell=True)    
-        def volume_down_top_left():
-            subprocess.run("pactl set-sink-volume "+Top_left_card+" -5% & pid=$!",shell=True)
+    def volume_up_top_left():
+        subprocess.run("pactl set-sink-volume "+Top_left_card+" +5% & pid=$!",shell=True)    
+    def volume_down_top_left():
+        subprocess.run("pactl set-sink-volume "+Top_left_card+" -5% & pid=$!",shell=True)
 
-        def volume_up_bottom_left():
-            subprocess.run("pactl set-sink-volume "+Bottom_left_card+" +5% & pid=$!",shell=True)    
-        def volume_down_bottom_left():
-            subprocess.run("pactl set-sink-volume "+Bottom_left_card+" -5% & pid=$!",shell=True)
+    def volume_up_bottom_left():
+        subprocess.run("pactl set-sink-volume "+Bottom_left_card+" +5% & pid=$!",shell=True)    
+    def volume_down_bottom_left():
+        subprocess.run("pactl set-sink-volume "+Bottom_left_card+" -5% & pid=$!",shell=True)
 
-        try :
-            record_dpy = display.Display()
+    try :
+        record_dpy = display.Display()
 
-            ctx = record_dpy.record_create_context(
-                    0,
-                    [record.AllClients],
-                    [{
-                            'core_requests': (0, 0),
-                            'core_replies': (0, 0),
-                            'ext_requests': (0, 0, 0, 0),
-                            'ext_replies': (0, 0, 0, 0),
-                            'delivered_events': (0, 0),
-                            'device_events': (X.KeyPress, X.MotionNotify),
-                            'errors': (0, 0),
-                            'client_started': False,
-                            'client_died': False,
-                    }])
+        ctx = record_dpy.record_create_context(
+                0,
+                [record.AllClients],
+                [{
+                        'core_requests': (0, 0),
+                        'core_replies': (0, 0),
+                        'ext_requests': (0, 0, 0, 0),
+                        'ext_replies': (0, 0, 0, 0),
+                        'delivered_events': (0, 0),
+                        'device_events': (X.KeyPress, X.MotionNotify),
+                        'errors': (0, 0),
+                        'client_started': False,
+                        'client_died': False,
+                }])
 
-            def record_callback(reply):
+        def record_callback(reply):
 
-                data = reply.data
-                while len(data):
-                    event, data = rq.EventField(None).parse_binary_value(data, record_dpy.display, None, None)
+            data = reply.data
+            while len(data):
+                event, data = rq.EventField(None).parse_binary_value(data, record_dpy.display, None, None)
+                
+                if event.type == X.ButtonPress:
+                    print (event.detail, event.root_x, event.root_y)
+
+                    # Let's set up right up corner 
+
+                    if all( [event.root_x > Screen_resolution [0] - Corner_area, event.root_y < Corner_area] ):
+
+                       print ("right up corner detected") 
+
+                       # event.detail 4 means wheel up event
+
+                       if event.detail == 4 : 
+                          print ("volume up!")
+                          volume_up_top_right() 
+
+                       # event.detail 5 means wheel down event
+
+                       if event.detail == 5 :
+                          print ("volume down!")
+                          volume_down_top_right()
+
+                    # Let's set up left up corner
+
+                    if all( [event.root_x < Corner_area , event.root_y < Corner_area] ):
+
+                       print ("left up corner detected")
+
+                       if event.detail == 4 :
+                          print ("volume up!")
+                          volume_up_top_left()
+
+                       if event.detail == 5 :
+                          print ("volume down!")
+                          volume_down_top_left() 
+          
+                    # Let's set up right down corner
+
+                    if all( [event.root_x > Screen_resolution[0] - Corner_area, event.root_y > Screen_resolution[1] - Corner_area] ):
+
+                       print ("right down corner detected")
+
+                       if event.detail == 4 :
+                          print ("volume up!")
+                          volume_up_bottom_right() 
+
+                       if event.detail == 5 :
+                          print ("volume down!")
+                          volume_down_bottom_right() 
+
+                    # Let's set up left down corner 
+
+                    if all( [event.root_x < Corner_area , event.root_y > Screen_resolution[1] - Corner_area] ):
+
+                       print ("left down corner detected")
+
+                       if event.detail == 4 :
+                          print ("volume up!") 
+                          volume_up_bottom_left() 
+
+                       if event.detail == 5 :
+                          print ("volume down!") 
+                          volume_down_bottom_left() 
+                                           
                     
-                    if event.type == X.ButtonPress:
-                        print (event.detail, event.root_x, event.root_y)
+                elif event.type == X.ButtonRelease:
 
-                        # Let's set up right up corner 
+                    print  (event.detail, event.root_x, event.root_y)
+                    
+                elif event.type == X.MotionNotify:
 
-                        if all( [event.root_x > Screen_resolution [0] - Corner_area, event.root_y < Corner_area] ):
+                    print (event.root_x, event.root_y)
+                    
 
-                           print ("right up corner detected") 
-
-                           # event.detail 4 means wheel up event
-
-                           if event.detail == 4 : 
-                              print ("volume up!")
-                              volume_up_top_right() 
-
-                           # event.detail 5 means wheel down event
-
-                           if event.detail == 5 :
-                              print ("volume down!")
-                              volume_down_top_right()
-
-                        # Let's set up left up corner
-
-                        if all( [event.root_x < Corner_area , event.root_y < Corner_area] ):
-
-                           print ("left up corner detected")
-
-                           if event.detail == 4 :
-                              print ("volume up!")
-                              volume_up_top_left()
-
-                           if event.detail == 5 :
-                              print ("volume down!")
-                              volume_down_top_left() 
-              
-                        # Let's set up right down corner
-
-                        if all( [event.root_x > Screen_resolution[0] - Corner_area, event.root_y > Screen_resolution[1] - Corner_area] ):
-
-                           print ("right down corner detected")
-
-                           if event.detail == 4 :
-                              print ("volume up!")
-                              volume_up_bottom_right() 
-
-                           if event.detail == 5 :
-                              print ("volume down!")
-                              volume_down_bottom_right() 
-
-                        # Let's set up left down corner 
-
-                        if all( [event.root_x < Corner_area , event.root_y > Screen_resolution[1] - Corner_area] ):
-
-                           print ("left down corner detected")
-
-                           if event.detail == 4 :
-                              print ("volume up!") 
-                              volume_up_bottom_left() 
-
-                           if event.detail == 5 :
-                              print ("volume down!") 
-                              volume_down_bottom_left() 
-                                               
-                        
-                    elif event.type == X.ButtonRelease:
-
-                        print  (event.detail, event.root_x, event.root_y)
-                        
-                    elif event.type == X.MotionNotify:
-
-                        print (event.root_x, event.root_y)
-                        
-
-            record_dpy.record_enable_context(ctx, record_callback)
-        except Exception as error:
-            print(error)
-            exit(1)
+        record_dpy.record_enable_context(ctx, record_callback)
+    except Exception as error:
+        print(error)
+        exit(1)
 
 
 
